6a7ebc4078a2bd5d414d0d3a6813ddb3a7cfc170
Melvin Kucuk <melvin.kucuk@ext.airbnb.com>
2024-06-19 09:37:35 -0300

    Create basic screen

---
+++ app/src/main/java/com/melvin/compass/presentation/MainScreen.kt
@@ -0,0 +1,24 @@
+package com.melvin.compass.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            Text(text = "Hello, World!")
+        }
+    }
+}
